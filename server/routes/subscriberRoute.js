8409c4c8690e78eddc530523c6b8c54505ca3c95
KeyCode <agyeis6000@gmail.com>
2025-04-07 07:56:04 -0700

    subscriber filename changed from Subscriber to subscriber...starting uppercase S to lowercase s

--- server/routes/subscriberRoute.js
+++ server/routes/subscriberRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Subscriber = require('../models/Subscriber');
+const Subscriber = require('../models/subscriber');
 const nodemailer = require('nodemailer');
 
 // Nodemailer transporter
